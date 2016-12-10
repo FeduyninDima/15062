008b97263e8fc65721ad7c2abbeeed6de22a35bd
Федюнин Дмитрий <15062@mail.ru>
2016-12-10 23:56:24 +0300

    Update Программа 1.c

--- Программа 1.c
+++ Программа 1.c
@@ -1,27 +1,27 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    int i,j,k,n,a[30];
-    cout<<"How many elements?";
-    cin>>n;
-    cout<<"\nEnter elements of array\n";
-    for(i=0;i<n;i++)
-        cin>>a[i];
-    for(i=0;i<n;i++)
-        for(j=i+1;j<n;)
-        {
-            if(a[i]==a[j])
-            {
-                for(k=j;k<n-1;k++)
-                    a[k]=a[k+1];
-                n--;
-            }
-            else
-                j++;
-        }
-    cout<<"\n";
-    for(i=0;i<n;i++)
-        cout<<a[i]<<" ";
-    return 0;
+
+#include "iostream"
+void delete_duplicates(int arr[], int &n){ // Зодача 1
+  for(int i=0;i<n;i++){
+     for(int j=i+1;j<n;) {
+         if(arr[i]==arr[j])
+         {
+             for(int k=j;k<n;k++)
+             {
+                 arr[k]=arr[k+1];
+             }
+             n--;
+         }
+         else j++;
+     }
+   }
+}
+int main() {
+  int n = 0;
+  std::cin >> n;
+  int m[n] = {-1};
+  for(int i = 0; i<n; i++)
+    std::cin >> m[i];
+  delete_duplicates(m, n);
+  for(int i=0;i<n;i++) std::cout <<m[i]<<" ";
+  return 0;
 }
