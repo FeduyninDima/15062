69fed1d47b993279f9cae4a4fc31a1660733ac90
Федюнин Дмитрий <15062@mail.ru>
2016-12-21 17:04:42 +0300

    Update brute.c

--- brute.c
+++ brute.c
@@ -1,16 +1,33 @@
-#include <iostream>
-int s;
-void printArr(int arr[], int n) {for(int i=0; i<n; i++) std::cout <<arr[i]<< " "; std::cout << std::endl;}
-void brf(int arr[], int n){
-  for(int i=0; i<10;i++) {
-    arr[s-n] = i;
-    printArr(arr, s);
-    if(n != 1) brf(arr, n-1);
-  }
+#include <bits/stdc++.h>
+
+using namespace std;
+template<class T>
+void QuickSort(vector<T>&v,int l,int r)
+{
+	T z=v[(l+r)/2];
+	int ll=l, rr=r;
+	while (ll<=rr)
+        {
+		     while (v[ll]>z) ll++;
+		     while (v[rr]<z) rr--;
+		     if (ll<=rr) swap(v[ll++], v[rr--]);
+	    }
+	if (l<rr) QuickSort(v,l,rr);
+	if (ll<r) QuickSort(v,ll,r);
 }
-int main (){
-  s = 0;
-  std::cin >> s;
-  int a[s]={0};
-  brf(a, s);
+
+int main()
+{
+	int n;
+	cin>>n;
+	vector<double> v(n);
+	for (int i=0; i<n; i++)
+        {
+		   cin>>v[i];
+	    }
+	QuickSort(v,0,v.size()-1);
+	for (int i=0; i<n; i++)
+	{
+        cout<<v[i]<<' ';
+	}
 }
