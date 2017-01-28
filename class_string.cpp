dbdc3056e6af8070215ef4f6be8a7802f6bff6fc
Федюнин Дмитрий <15062@mail.ru>
2017-01-28 23:23:32 +0300

    Update class_string.cpp

--- class_string.cpp
+++ class_string.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
- 
+#include <cstring>
+
 using namespace std;
  
 class String  {
@@ -7,37 +8,89 @@ private:
     char *pStr;
     int _size;
 public:
-    String()  {						//конструктор
+    String()  {
         pStr = NULL;
         _size = 0;
     }
-
-	~String()  {					//деструктор
-        delete []pStr;
+    String(char *str)  {
+        _size = strlen(str) + 1;
+        pStr = new char[_size];
+        strcpy(pStr, str);
     }
-	
     int size()  {
         return _size;
     }
+    ~String()  {
+        delete []pStr;
+    }
+	
+	void print()
+	{	
+	for(int i=0; i<_size; i++)
+		cout<<pStr[i];
+	}
 
-	char get(int i)	{
+	char get(int i)
+	{
 	return pStr[i];
 	}
 
-	void set(int i)	{
-	pStr[i] = //.....
-	}
-	
-	void print	{
-	for(int i=0; i<strlen(pStr); i++)
-	cout<<pStr[i];
+	void set(int i, char c)
+	{
+	pStr[i] = c;
 	}
 
+	int find(String str)
+	{
+	bool ch = false;
+	int index;
+	int ind1=0;
+	for(int i = 0; i < _size; i++)
+		{
+			if (!ch)
+			{
+				if(pStr[i] == str.get(0))
+				ch = true;
+				index = i;
+				ind1 = 1;
+			}
+				else
+			if (ch)
+			{
+				if (!pStr[i] == str.get(ind1))
+					{
+					ch = false; 
+					index = 0; 
+					ind1 = 0;
+					}
+			}
+		}
+	return index;
+	}
+
+	void Concat(String s)
+	{
+     _size += s.size();
+     char* tmp = new char[_size + 1];
+     ::strcpy(tmp, pStr);
+     ::strcat(tmp, s.pStr);
+     delete [] pStr;
+     pStr = tmp;
+	}
+
+	
 };
  
-int main()  
-{
+
+
+
+int main()  {
+	String str1("kekekek.");
     String str("Blablabla");
-    String.print(str);
-    return 0;
+	str.Concat(str1);
+	cout<<str.find("abl")<<endl;
+	str.print();
+	cout<<'.';
+
+	return 0;
 }
